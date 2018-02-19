d8696974e591f4b7142d3d44b30f0a523c0f0878
ZhuoQ <zl14632@my.bristol.ac.uk>
2018-02-19 14:54:46 +0000

    Drawing Points

--- rasteriser/Source/skeleton.cpp
+++ rasteriser/Source/skeleton.cpp
@@ -1,69 +1,165 @@
 #include <iostream>
+
 #include <glm/glm.hpp>
+
 #include <SDL.h>
+
 #include "SDLauxiliary.h"
+
 #include "TestModelH.h"
+
 #include <stdint.h>
 
+
+
 using namespace std;
+
 using glm::vec3;
+
 using glm::mat3;
+
 using glm::vec4;
+
 using glm::mat4;
 
-// test
+using glm::ivec2;
+
+
+
+
+
 #define SCREEN_WIDTH 320
+
 #define SCREEN_HEIGHT 256
+
 #define FULLSCREEN_MODE false
 
+
+
 /* ----------------------------------------------------------------------------*/
+
 /* FUNCTIONS                                                                   */
 
+
+
 void Update();
-void Draw(screen* screen);
+
+void Draw(screen* screen, const vector<Triangle>& triangles);
+
+void VertexShader(const vec4& v, ivec2& p);
+
+
 
 int main( int argc, char* argv[] )
+
 {
-  
+
+  vector<Triangle> triangles;
+
+  LoadTestModel( triangles );
+
   screen *screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN_MODE );
+
   
+
   while( NoQuitMessageSDL() )
+
     {
+
       Update();
-      Draw(screen);
+
+      Draw(screen,triangles);
+
       SDL_Renderframe(screen);
+
     }
+
+
 
   SDL_SaveImage( screen, "screenshot.bmp" );
 
+
+
   KillSDL(screen);
+
   return 0;
+
 }
+
+
 
 /*Place your drawing here*/
-void Draw(screen* screen)
+
+void Draw(screen* screen,const vector<Triangle>& triangles)
+
 {
+
   /* Clear buffer */
+
   memset(screen->buffer, 0, screen->height*screen->width*sizeof(uint32_t));
+
   
-  vec3 colour(1.0,0.0,0.0);
-  for(int i=0; i<1000; i++)
-    {
-      uint32_t x = rand() % screen->width;
-      uint32_t y = rand() % screen->height;
-      PutPixelSDL(screen, x, y, colour);
-    }
+
+  for( uint32_t i=0; i<triangles.size(); ++i ){
+
+    vector<vec4> vertices(3);
+
+    vertices[0] = triangles[i].v0;
+
+    vertices[1] = triangles[i].v1;
+
+    vertices[2] = triangles[i].v2;
+
+      for(int v=0; v<3; ++v){
+
+        ivec2 projPos;
+
+	VertexShader( vertices[v], projPos );
+
+	vec3 color(1,1,1);
+
+	PutPixelSDL( screen, projPos.x, projPos.y, color );
+
+      }
+
+  }
+
 }
 
+
+
 /*Place updates of parameters here*/
+
 void Update()
+
 {
+
   static int t = SDL_GetTicks();
+
   /* Compute frame time */
+
   int t2 = SDL_GetTicks();
+
   float dt = float(t2-t);
+
   t = t2;
+
   /*Good idea to remove this*/
+
   std::cout << "Render time: " << dt << " ms." << std::endl;
+
   /* Update variables*/
+
+}
+
+
+
+void VertexShader(const vec4& v, ivec2& p){
+
+  int f = SCREEN_HEIGHT/4; // focalLength
+
+  p.x = (f*v.x/v.z) + (SCREEN_WIDTH/2);
+
+  p.y = (f*v.y/v.z) + (SCREEN_HEIGHT/2);
+
 }
